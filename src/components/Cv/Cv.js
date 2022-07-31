8094c42fe1933c1a9c408d8d3bced67028c1e10e
Lucas <Lucasjg696@gmail.com>
2022-07-31 05:22:39 -0300

    algo

--- src/components/Cv/Cv.js
+++ src/components/Cv/Cv.js
@@ -1,13 +1,24 @@
 import React from "react"
 import Footer from "../Footer/Footer"
 import NavBar from "../NavBar/NavBar"
+import style from "./Cv.module.css"
 
 export default function Cv(){
     return(
         <div>
             <NavBar/>
+            <div className={style.imgC}>
+                <img src={"https://res.cloudinary.com/dbgreenshop/image/upload/v1659250680/CV_1.0_nzo390.png"} alt="cv" className={style.img}/>
+            </div>
             <div>
-                Mi curriculum :v
+                <div className={style.logoC}>
+                    <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="logo" className={style.logo}/>
+                    <img src="https://www.pngmart.com/files/21/Linkedin-In-Logo-PNG-Isolated-Image.png" alt="logo" className={style.logo}/>
+                    <img src="https://logodownload.org/wp-content/uploads/2015/04/whatsapp-logo-4.png" alt="logo" className={style.logo}/>
+                    <a href="mailto:lucasjg696@gmail.com">
+                        <img src="https://icones.pro/wp-content/uploads/2021/03/icone-gmail-logo-png-noire.png" alt="logo" className={style.logo}/>
+                    </a>
+                </div>
             </div>
             <Footer/>
         </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,7 +26,7 @@ export default function Projects(){
             description:"Functional ecommerce with a cannabis theme, it has a shopping cart, Mercado pago, register and login with firebase, a relational database that saves user purchases and an administrator panel."
         },
         {
-            link:"https://pf-eccomerce.vercel.app/",
+            link:"https://portafolio-umber-eta.vercel.app/",
             url:'https://images.fineartamerica.com/images/artworkimages/medium/2/retro-80s-90s-vaporwave-palm-trees-gift-pink-blue-design-design-dc-designs-suamaceir-transparent.png',
             name:"This portafolio",
             description:"It is a small project that has just started but I hope to be able to expand with my skills over time"
