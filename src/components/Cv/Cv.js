5f7384cfcd085cce3658e9295e9eeffc29bd764c
Lucas <Lucasjg696@gmail.com>
2022-08-02 06:44:41 -0300

    linkedin

--- src/components/Cv/Cv.js
+++ src/components/Cv/Cv.js
@@ -15,7 +15,7 @@ export default function Cv(){
                     <a href="https://github.com/Luksjg" rel="noreferrer" target="_blank">
                         <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="logo" className={style.logo}/>
                     </a>
-                    <a href="https://www.linkedin.com/in/lucas-j-gomez-151915182/" rel="noreferrer" target="_blank">
+                    <a href="https://www.linkedin.com/in/lucas-j-gomez/" rel="noreferrer" target="_blank">
                         <img src="https://www.pngmart.com/files/21/Linkedin-In-Logo-PNG-Isolated-Image.png" alt="logo" className={style.logo}/>
                     </a>
                     <a href="https://wa.me/541144405959" rel="noreferrer" target="_blank">
