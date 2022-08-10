ed78834ef2cc08ed23008b2407311bd039abcb3f
Lucas <Lucasjg696@gmail.com>
2022-08-10 12:33:18 -0300

    sun change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer(){
 
     return(
         <div className={style.container}>
-            <img src="https://images.fineartamerica.com/images/artworkimages/medium/2/t-500-108-vaporwave-sunset-mountains-scenery-dc-designs-suamaceir-transparent.png" alt='img2' className={style.sunfooter}/>
+            <img src="https://un-defined.hotglue.me/?start.head.156825781149" alt='img2' className={style.sunfooter}/>
             <img src='https://www.pngkit.com/png/full/100-1005708_mountains-grid-hearst-castle-neptune-pool.png' alt='img' width="100%" className={style.imgfooter} ></img>
         </div>
     )    
